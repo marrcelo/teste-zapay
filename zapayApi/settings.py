073f581c05ff602dda030a2cdba156988e98ac41
Marcelo Magalhães <magalhaesmarcelo@outlook.com.br>
2019-11-18 21:35:25 -0300

    configurando deploy

--- zapayApi/settings.py
+++ zapayApi/settings.py
@@ -122,4 +122,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
